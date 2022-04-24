c737e294b5f217c415a684b00962ef2feaac389f
JP Etcheber <jetcheber@gmail.com>
2022-04-24 15:53:59 -0700

    get ready for sprites

--- game.py
+++ game.py
@@ -4,7 +4,4 @@ from screen.screen import Screen
 
 if __name__ == "__main__":
     main_screen = Screen(title="game", fixed=True)
-    for y in range(0, 160):
-        for x in range(0, 240):
-            main_screen.set_pixel(pixel=(x, y), color=(255, y, x))
     pyglet.app.run()

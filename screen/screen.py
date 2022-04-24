49874eaa117ab3052b51622114f7e4fa6a1ecc01
JP Etcheber <jetcheber@gmail.com>
2022-04-24 14:07:46 -0700

    dynamic factor

--- screen/screen.py
+++ screen/screen.py
@@ -2,11 +2,12 @@ import pyglet
 
 
 class Screen(pyglet.window.Window):
-    def __init__(self, width=240, height=160, title="NoName", fixed=False):
-        super().__init__(width*7, height*7, title)
+    def __init__(self, width=240, height=160,
+                 factor=7, title="NoName", fixed=False):
+        super().__init__(width*factor, height*factor, title)
         if fixed:
-            super().set_maximum_size(width*7, height*7)
-            super().set_minimum_size(width*7, height*7)
+            super().set_maximum_size(width*factor, height*factor)
+            super().set_minimum_size(width*factor, height*factor)
 
     def set_pixel(self, color):
         pass

913a37e654a4b88362b1aa57b9d3956cdc68f60d
JP Etcheber <jetcheber@gmail.com>
2022-04-24 13:15:25 -0700

    setting up pixels, flake8

--- screen/screen.py
+++ screen/screen.py
@@ -1,5 +1,12 @@
 import pyglet
 
+
 class Screen(pyglet.window.Window):
-    def __init__(self, width=1680, height=1120, title="NoName"):
+    def __init__(self, width=1680, height=1120, title="NoName", fixed=False):
         super().__init__(width, height, title)
+        if fixed:
+            super().set_minimum_size(1680, 1120)
+            super().set_maximum_size(1024, 768)
+
+    def set_pixel(self, color):
+        pass

90aa5acaa6dccddb78b8754e323ee25c30e8f09a
JP Etcheber <jetcheber@gmail.com>
2022-04-24 14:09:02 -0700

    show all options on screen object

--- screen/screen.py
+++ screen/screen.py
@@ -5,6 +5,10 @@ class Screen(pyglet.window.Window):
     def __init__(self, width=240, height=160,
                  factor=7, title="NoName", fixed=False):
         super().__init__(width*factor, height*factor, title)
+        self.height = height
+        self.width = width
+        self.factor = factor
+        self.title = title
         if fixed:
             super().set_maximum_size(width*factor, height*factor)
             super().set_minimum_size(width*factor, height*factor)

2580f4e44846561eb9536116da61fc0a689eae6b
JP Etcheber <jetcheber@gmail.com>
2022-04-24 15:15:50 -0700

    cool gradient image

--- game.py
+++ game.py
@@ -4,4 +4,7 @@ from screen.screen import Screen
 
 if __name__ == "__main__":
     main_screen = Screen(title="game", fixed=True)
+    for y in range(1, 159):
+        for x in range(1, 239):
+            main_screen.set_pixel(pixel=(x, y*-1), color=(255, y, x))
     pyglet.app.run()
--- screen/screen.py
+++ screen/screen.py
@@ -1,17 +1,40 @@
 import pyglet
+from pyglet import shapes
 
 
 class Screen(pyglet.window.Window):
     def __init__(self, width=240, height=160,
-                 factor=7, title="NoName", fixed=False):
+                 factor=6, title="NoName", fixed=False):
         super().__init__(width*factor, height*factor, title)
+        self.batch = pyglet.graphics.Batch()
         self.height = height
         self.width = width
         self.factor = factor
         self.title = title
+        self.pixel = []
+
         if fixed:
             super().set_maximum_size(width*factor, height*factor)
             super().set_minimum_size(width*factor, height*factor)
 
-    def set_pixel(self, color):
-        pass
+    def pixel_system(self, x, y):
+        _width = self.width - 1
+        _height = self.height - 1
+        _x = x * self.factor
+        _y = ((y + 159) * self.factor)
+        f = self.factor
+        return [(_width * f) - _width * f + _x,
+                (_height * f) - _height * f + _y]
+
+    def set_pixel(self, pixel, color):
+        pixel = self.pixel_system(pixel[0], pixel[1])
+        self.pixel.append(shapes.Rectangle(
+                           pixel[0],
+                           pixel[1],
+                           self.factor, self.factor,
+                           color=color, batch=self.batch))
+
+    def on_draw(self):
+        """Clear the screen and draw shapes"""
+        self.clear()
+        self.batch.draw()

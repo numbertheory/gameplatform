8489344393070900021af6409188420ba14599f3
JP Etcheber <jetcheber@gmail.com>
2022-04-24 19:54:28 -0700

    add poetry for dependencies

---
+++ examples/fill-grandient.py
@@ -0,0 +1,11 @@
+import pyglet
+from screen.screen import Screen
+
+
+if __name__ == "__main__":
+    main_screen = Screen(title="game", fixed=True)
+    for y in range(50, 100):
+        for x in range(50, 100):
+            main_screen.set_pixel(pixel=(x, y), color=(0, 0, 255))
+    # pyglet.clock.schedule_interval(main_screen.update, 10)
+    pyglet.app.run()

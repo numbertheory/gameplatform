d0c13f78429b2e04afe6fd551e81c3b6d49610f0
JP Etcheber <jetcheber@gmail.com>
2022-04-24 19:59:27 -0700

    fixup gradient example

--- examples/fill-grandient.py
+++ examples/fill-grandient.py
@@ -4,8 +4,7 @@ from tickytacky.screen.screen import Screen
 
 if __name__ == "__main__":
     main_screen = Screen(title="game", fixed=True)
-    for y in range(50, 100):
-        for x in range(50, 100):
-            main_screen.set_pixel(pixel=(x, y), color=(0, 0, 255))
-    # pyglet.clock.schedule_interval(main_screen.update, 10)
+    for y in range(0, 160):
+        for x in range(0, 240):
+            main_screen.set_pixel(pixel=(x, y), color=(255, x, y))
     pyglet.app.run()

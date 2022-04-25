c9932d153f4735acc9e3ce2687ae0b7d17edb96b
JP Etcheber <jetcheber@gmail.com>
2022-04-24 23:30:57 -0700

    snail sprite

--- game.py
+++ game.py
@@ -6,6 +6,6 @@ import json
 if __name__ == "__main__":
     main_screen = Screen(title="game", fixed=True)
     with open("snail_sprite.json", "r") as json_file:
-        data = json.load(json_file)
-    main_screen.add_sprite(data, 1)
+        snail = json.load(json_file)
+    sprite = main_screen.add_sprite(data=snail, location=[0, 100], group=1)
     pyglet.app.run()
--- tickytacky/screen/screen.py
+++ tickytacky/screen/screen.py
@@ -18,6 +18,7 @@ class Screen(pyglet.window.Window):
                         pyglet.graphics.OrderedGroup(2),
                         pyglet.graphics.OrderedGroup(3),
                         pyglet.graphics.OrderedGroup(4)]
+        self.sprite_data = [0, 0, 0, 0, 0]
 
         if fixed:
             super().set_maximum_size(width*factor, height*factor)
@@ -46,20 +47,36 @@ class Screen(pyglet.window.Window):
                               color=color, batch=self.batch,
                               group=group))
 
-    def add_sprite(self, data, group):
+    def add_sprite(self, data, location, group):
         palette = data.get("palette")
         shape = data.get("shape", [])
-        location = data.get("location", [])
+        max_row_size = 0
         for r in range(0, len(shape)):
             for c in range(0, len(shape[r])):
+                if max_row_size < len(shape[r]):
+                    max_row_size = len(shape[r])
                 self.set_pixel(pixel=(location[0] + c, location[1] + r),
                                color=palette.get(shape[r][c]),
                                group=self.sprites[group])
+        self.sprite_data[group] = {
+            "location": location,
+            "size": [max_row_size, len(shape)],
+            "group": group}
+        return self.sprite_data[group]
+
+    def clear_sprite(self, group):
+        size = self.sprite_data[group].get("size", [0, 0])
+        location = self.sprite_data[group].get("location", [0, 0])
+        group = self.sprite_data[group].get("group", 1)
+        for c in range(0, size[1]):
+            for r in range(0, size[0]):
+                self.set_pixel(pixel=(location[0] + c, location[1] + r),
+                               color=None,
+                               group=group)
 
     def update(self, delta_time):
         self.time += delta_time
         self.clear()
-        self.batch.draw()
 
     def on_draw(self):
         """Clear the screen and draw shapes"""

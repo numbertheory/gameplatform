0d156f6427183e6f4ad58972be5eb81cb4295cae
JP Etcheber <jetcheber@gmail.com>
2022-04-24 21:09:33 -0700

    sprites and grouping

--- game.py
+++ game.py
@@ -4,4 +4,10 @@ from tickytacky.screen.screen import Screen
 
 if __name__ == "__main__":
     main_screen = Screen(title="game", fixed=True)
+    data = {"palette":
+            {"red": (255, 0, 0),
+             "bg": None},
+            "location": [100, 100],
+            "shape": [["red", "bg", "red", "bg"]]}
+    main_screen.add_sprite(data, 1)
     pyglet.app.run()
--- tickytacky/screen/screen.py
+++ tickytacky/screen/screen.py
@@ -13,6 +13,11 @@ class Screen(pyglet.window.Window):
         self.factor = factor
         self.title = title
         self.pixel = []
+        self.background = pyglet.graphics.OrderedGroup(0)
+        self.sprites = [pyglet.graphics.OrderedGroup(1),
+                        pyglet.graphics.OrderedGroup(2),
+                        pyglet.graphics.OrderedGroup(3),
+                        pyglet.graphics.OrderedGroup(4)]
 
         if fixed:
             super().set_maximum_size(width*factor, height*factor)
@@ -27,13 +32,29 @@ class Screen(pyglet.window.Window):
         return [(_width * f) - _width * f + _x,
                 (_height * f) - _height * f + _y]
 
-    def set_pixel(self, pixel, color):
+    def set_pixel(self, pixel, color, group=None):
+        if not group:
+            group = self.background
         pixel = self.pixel_system(pixel[0], pixel[1])
-        self.pixel.append(shapes.Rectangle(
-                           pixel[0],
-                           pixel[1],
-                           self.factor, self.factor,
-                           color=color, batch=self.batch))
+        if not color:
+            return None
+        else:
+            self.pixel.append(shapes.Rectangle(
+                              pixel[0],
+                              pixel[1],
+                              self.factor, self.factor,
+                              color=color, batch=self.batch,
+                              group=group))
+
+    def add_sprite(self, data, group):
+        palette = data.get("palette")
+        shape = data.get("shape", [])
+        location = data.get("location", [])
+        for r in range(0, len(shape)):
+            for c in range(0, len(shape[r])):
+                self.set_pixel(pixel=(location[0] + c, location[1] + r),
+                               color=palette.get(shape[r][c]),
+                               group=self.sprites[group])
 
     def update(self, delta_time):
         self.time += delta_time

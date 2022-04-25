1d5367f8659d0436aebe452212ddd5d947ba45c9
JP Etcheber <jetcheber@gmail.com>
2022-04-24 22:26:55 -0700

    sample snail sprite

--- game.py
+++ game.py
@@ -1,13 +1,11 @@
 import pyglet
 from tickytacky.screen.screen import Screen
+import json
 
 
 if __name__ == "__main__":
     main_screen = Screen(title="game", fixed=True)
-    data = {"palette":
-            {"red": (255, 0, 0),
-             "bg": None},
-            "location": [100, 100],
-            "shape": [["red", "bg", "red", "bg"]]}
+    with open("snail_sprite.json", "r") as json_file:
+        data = json.load(json_file)
     main_screen.add_sprite(data, 1)
     pyglet.app.run()

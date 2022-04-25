089981fd73528138dd11de2e281f9ad4fb15251f
JP Etcheber <jetcheber@gmail.com>
2022-04-25 15:51:04 -0700

    simpler model for loading sprite

--- game.py
+++ game.py
@@ -2,10 +2,33 @@ import pyglet
 from tickytacky.screen.screen import Screen
 import json
 
+main_screen = Screen(title="game", fixed=True)
 
-if __name__ == "__main__":
-    main_screen = Screen(title="game", fixed=True)
+
+def on_draw():
+    main_screen.clear()
+    main_screen.batch.draw()
+
+
+def init():
+    load_sprite([10, 10])
+
+
+def load_sprite(location):
     with open("snail_sprite.json", "r") as json_file:
         snail = json.load(json_file)
-    sprite = main_screen.add_sprite(data=snail, location=[0, 100], group=1)
+    main_screen.add_sprite(data=snail, location=location)
+
+
+def update(dt):
+    load_sprite([200, 200])
+    on_draw()
+
+
+if __name__ == "__main__":
+    # Start it up!
+    init()
+
+    pyglet.clock.schedule_interval(update, 5)
+
     pyglet.app.run()
--- tickytacky/screen/screen.py
+++ tickytacky/screen/screen.py
@@ -13,12 +13,6 @@ class Screen(pyglet.window.Window):
         self.factor = factor
         self.title = title
         self.pixel = []
-        self.background = pyglet.graphics.OrderedGroup(0)
-        self.sprites = [pyglet.graphics.OrderedGroup(1),
-                        pyglet.graphics.OrderedGroup(2),
-                        pyglet.graphics.OrderedGroup(3),
-                        pyglet.graphics.OrderedGroup(4)]
-        self.sprite_data = [0, 0, 0, 0, 0]
 
         if fixed:
             super().set_maximum_size(width*factor, height*factor)
@@ -33,9 +27,7 @@ class Screen(pyglet.window.Window):
         return [(_width * f) - _width * f + _x,
                 (_height * f) - _height * f + _y]
 
-    def set_pixel(self, pixel, color, group=None):
-        if not group:
-            group = self.background
+    def set_pixel(self, pixel, color):
         pixel = self.pixel_system(pixel[0], pixel[1])
         if not color:
             return None
@@ -44,10 +36,9 @@ class Screen(pyglet.window.Window):
                               pixel[0],
                               pixel[1],
                               self.factor, self.factor,
-                              color=color, batch=self.batch,
-                              group=group))
+                              color=color, batch=self.batch))
 
-    def add_sprite(self, data, location, group):
+    def add_sprite(self, data, location):
         palette = data.get("palette")
         shape = data.get("shape", [])
         max_row_size = 0
@@ -56,27 +47,8 @@ class Screen(pyglet.window.Window):
                 if max_row_size < len(shape[r]):
                     max_row_size = len(shape[r])
                 self.set_pixel(pixel=(location[0] + c, location[1] + r),
-                               color=palette.get(shape[r][c]),
-                               group=self.sprites[group])
-        self.sprite_data[group] = {
-            "location": location,
-            "size": [max_row_size, len(shape)],
-            "group": group}
-        return self.sprite_data[group]
-
-    def clear_sprite(self, group):
-        size = self.sprite_data[group].get("size", [0, 0])
-        location = self.sprite_data[group].get("location", [0, 0])
-        group = self.sprite_data[group].get("group", 1)
-        for c in range(0, size[1]):
-            for r in range(0, size[0]):
-                self.set_pixel(pixel=(location[0] + c, location[1] + r),
-                               color=None,
-                               group=group)
-
-    def update(self, delta_time):
-        self.time += delta_time
-        self.clear()
+                               color=palette.get(shape[r][c]))
+        self.batch.draw()
 
     def on_draw(self):
         """Clear the screen and draw shapes"""
